6c2484faedbddd18eea2eabb23732ae03d220c55
Tomoyoshi Kimura <tomoyoshikimura@tomoyoshki.local>
2022-04-17 10:05:55 -0500

    Updated sample main coding style

--- analysis/sample_main.py
+++ analysis/sample_main.py
@@ -1,15 +1,24 @@
+'''Sample main file'''
 from compare_strategy import CompareStrategy
 from strategy_analysis import StrategyAnalysis
 
-fill_path = "sample_data/BACK_TKSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_fill.csv"
-order_path = "sample_data/BACK_TKSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_order.csv"
-pnl_path = "sample_data/BACK_TKSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_pnl.csv"
-my_strategy1 = StrategyAnalysis(fill_file=fill_path, order_file=order_path, pnl_file=pnl_path, initial_value=10000000)
+FILL_PATH1 = "sample_data/BACK_TKSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_fill.csv"
+ORDER_PATH1 = "sample_data/BACK_TKSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_order.csv"
+PNL_PATH1 = "sample_data/BACK_TKSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_pnl.csv"
+my_strategy1 = StrategyAnalysis(
+    fill_file=FILL_PATH1,
+    order_file=ORDER_PATH1,
+    pnl_file=PNL_PATH1,
+    initial_value=10000000)
 
-fill_path = "sample_data/BACK_NONSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_fill.csv"
-order_path = "sample_data/BACK_NONSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_order.csv"
-pnl_path = "sample_data/BACK_NONSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_pnl.csv"
-my_strategy2 = StrategyAnalysis(fill_file=fill_path, order_file=order_path, pnl_file=pnl_path, initial_value=10000000)
+FILL_PATH2 = "sample_data/BACK_NONSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_fill.csv"
+ORDER_PATH2 = "sample_data/BACK_NONSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_order.csv"
+PNL_PATH2 = "sample_data/BACK_NONSPY_2022-04-02_143804_start_10-30-2019_end_10-30-2019_pnl.csv"
+my_strategy2 = StrategyAnalysis(
+    fill_file=FILL_PATH2,
+    order_file=ORDER_PATH2,
+    pnl_file=PNL_PATH2,
+    initial_value=10000000)
 
 
 
