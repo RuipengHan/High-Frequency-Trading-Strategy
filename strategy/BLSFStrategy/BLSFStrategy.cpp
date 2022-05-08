61d92c91c84014f118f71b8213df5b5d1064bc36
Tomoyoshi Kimura <tomoyoshikimura@wirelessprv-10-192-135-128.near.illinois.edu>
2022-05-08 16:20:16 -0500

    Lint check

--- strategy/BLSFStrategy/BLSFStrategy.cpp
+++ strategy/BLSFStrategy/BLSFStrategy.cpp
@@ -21,41 +21,56 @@ using namespace RCM::StrategyStudio::Utilities;
 
 using namespace std;
 
-BLSFStrategy::BLSFStrategy(StrategyID strategyID, const std::string& strategyName, const std::string& groupName):
+BLSFStrategy::BLSFStrategy(
+        StrategyID strategyID,
+        const std::string& strategyName,
+        const std::string& groupName):
     Strategy(strategyID, strategyName, groupName),
     currentState(BUY),
     currentDate(0) {
 }
 
-BLSFStrategy::~BLSFStrategy(){}
+BLSFStrategy::~BLSFStrategy() {}
 
-
-void BLSFStrategy::RegisterForStrategyEvents(StrategyEventRegister* eventRegister, DateType currDate)
-{    
+void BLSFStrategy::RegisterForStrategyEvents(
+        StrategyEventRegister* eventRegister,
+        DateType currDate
+        ) {
     for (SymbolSetConstIter it = symbols_begin(); it != symbols_end(); ++it) {
         eventRegister->RegisterForBars(*it, BAR_TYPE_TIME, 10);
     }
 }
 
-void BLSFStrategy::OnTrade(const TradeDataEventMsg& msg)
-{
-
+void BLSFStrategy::OnTrade(const TradeDataEventMsg& msg) {
     date msg_date = msg.adapter_time().date();
-    // Sell at the beginning of the day
     if (currentState == SELL && msg_date != currentDate) {
         currentDate = msg_date;
-        std::cout << "OnTrade(): (" << msg.adapter_time() << "): " << msg.instrument().symbol() << ": " << msg.trade().size() << " @ $" << msg.trade().price() << std::endl;
-        this->SendTradeOrder(&msg.instrument(), -1); //sell one share every time there is a trade
+        std::cout << "OnTrade(): ("
+                    << msg.adapter_time()
+                    << "): "
+                    << msg.instrument().symbol()
+                    << ": " << msg.trade().size()
+                    << " @ $"
+                    << msg.trade().price()
+                    << std::endl;
+        this->SendTradeOrder(&msg.instrument(), -1);
         currentState = BUY;
     }
-    // Buy at the end of the day
     else {
         if(currentDate == date(0)) {
             currentDate = msg_date;
         }
         tm date_tm = to_tm(msg.adapter_time());
         if(currentState == BUY && date_tm.tm_hour == 19 && date_tm.tm_min >= 58) {
-            std::cout << "OnTrade(): (" << msg.adapter_time() << "): " << msg.instrument().symbol() << ": " << msg.trade().size() << " @ $" << msg.trade().price() << std::endl;
+            std::cout << "OnTrade(): ("
+                        << msg.adapter_time()
+                        << "): "
+                        << msg.instrument().symbol()
+                        << ": "
+                        << msg.trade().size()
+                        << " @ $"
+                        << msg.trade().price()
+                        << std::endl;
             this->SendTradeOrder(&msg.instrument(), 1);
             cout << date_tm.tm_hour << "\t" << date_tm.tm_min << endl;
             currentState = SELL;
@@ -63,12 +78,10 @@ void BLSFStrategy::OnTrade(const TradeDataEventMsg& msg)
     } 
 }
 
-void BLSFStrategy::OnBar(const BarEventMsg& msg)
-{
+void BLSFStrategy::OnBar(const BarEventMsg& msg) {
     date msg_date = msg.bar_time().date();
     if (currentState == SELL && msg_date != currentDate) {
         tm date_tm = to_tm(msg.bar_time());
-        // ensure the market begin time 
         if(currentState == BUY && date_tm.tm_hour < 13) {
             return;
         }
@@ -76,8 +89,12 @@ void BLSFStrategy::OnBar(const BarEventMsg& msg)
             return;
         } 
         currentDate = msg_date;
-        std::cout << "Sending BAR order: (" << msg.bar_time() << "): " << msg.instrument().symbol() << std::endl;
-        this->SendQuoteOrder(&msg.instrument(), -1); //sell one share every time there is a trade
+        std::cout << "Sending BAR order: ("
+                    << msg.bar_time()
+                    << "): "
+                    << msg.instrument().symbol()
+                    << std::endl;
+        this->SendQuoteOrder(&msg.instrument(), -1);
         currentState = BUY;
     }
     // Buy at the end of the day
@@ -87,35 +104,47 @@ void BLSFStrategy::OnBar(const BarEventMsg& msg)
         }
         tm date_tm = to_tm(msg.bar_time());
         if(currentState == BUY && date_tm.tm_hour == 19 && date_tm.tm_min >= 58) {
-            std::cout << "Sending BAR order: (" << msg.bar_time() << "): " << msg.instrument().symbol() << std::endl;
+            std::cout << "Sending BAR order: ("
+                        << msg.bar_time()
+                        << "): "
+                        << msg.instrument().symbol()
+                        << std::endl;
             this->SendQuoteOrder(&msg.instrument(), 1);
             currentState = SELL;
         }
     } 
 }
 
-void BLSFStrategy::OnOrderUpdate(const OrderUpdateEventMsg& msg)
-{    
- std::cout << "OnOrderUpdate(): " << msg.update_time() << " " << msg.name() << std::endl;
-    if(msg.completes_order())
-    {
-  std::cout << "OnOrderUpdate(): order is complete; " << std::endl;
+void BLSFStrategy::OnOrderUpdate(const OrderUpdateEventMsg& msg) {
+    std::cout << "OnOrderUpdate(): "
+                << msg.update_time()
+                << " "
+                << msg.name()
+                << std::endl;
+    if(msg.completes_order()) {
+        std::cout << "OnOrderUpdate(): order is complete; " << std::endl;
     }
 }
 
-void BLSFStrategy::SendTradeOrder(const Instrument* instrument, int trade_size)
-{
+void BLSFStrategy::SendTradeOrder(
+        const Instrument* instrument,
+        int trade_size) {
     double last_trade_price = instrument->last_trade().price();
     double price = trade_size > 0 ? last_trade_price : last_trade_price;
-
     OrderParams params(*instrument,
         abs(trade_size),
         price,
-        (instrument->type() == INSTRUMENT_TYPE_EQUITY) ? MARKET_CENTER_ID_IEX : ((instrument->type() == INSTRUMENT_TYPE_OPTION) ? MARKET_CENTER_ID_CBOE_OPTIONS : MARKET_CENTER_ID_CME_GLOBEX),
+        (instrument->type() == INSTRUMENT_TYPE_EQUITY) ? MARKET_CENTER_ID_IEX :
+            ((instrument->type() == INSTRUMENT_TYPE_OPTION) ?
+            MARKET_CENTER_ID_CBOE_OPTIONS : MARKET_CENTER_ID_CME_GLOBEX),
         (trade_size>0) ? ORDER_SIDE_BUY : ORDER_SIDE_SELL,
         ORDER_TIF_DAY,
         ORDER_TYPE_LIMIT);
-    std::cout << "SendTradeOrder(): about to send new order for " << trade_size << " at $" << price << std::endl;
+    std::cout << "SendTradeOrder(): about to send new order for "
+                << trade_size
+                << " at $"
+                << price
+                << std::endl;
     TradeActionResult tra = trade_actions()->SendNewOrder(params);
     if (tra == TRADE_ACTION_RESULT_SUCCESSFUL) {
         std::cout << "Sending new trade order successful!" << std::endl;
@@ -126,11 +155,21 @@ void BLSFStrategy::SendTradeOrder(const Instrument* instrument, int trade_size)
 }
 
 
-void BLSFStrategy::SendQuoteOrder(const Instrument* instrument, int trade_size)
-{
-    if(instrument->top_quote().ask()<.01 || instrument->top_quote().bid()<.01 || !instrument->top_quote().ask_side().IsValid() || !instrument->top_quote().ask_side().IsValid()) {
+void BLSFStrategy::SendQuoteOrder(
+        const Instrument* instrument,
+        int trade_size) {
+    if(instrument->top_quote().ask()<.01 ||
+            instrument->top_quote().bid()<.01 ||
+            !instrument->top_quote().ask_side().IsValid() ||
+            !instrument->top_quote().ask_side().IsValid()) {
         std::stringstream ss;
-        ss << "Sending buy order for " << instrument->symbol() << " at price " << instrument->top_quote().ask() << " and quantity " << trade_size <<" with missing quote data";   
+        ss << "Sending buy order for "
+            << instrument->symbol()
+            << " at price "
+            << instrument->top_quote().ask()
+            << " and quantity "
+            << trade_size
+            << " with missing quote data";
         logger().LogToClient(LOGLEVEL_DEBUG, ss.str());
         std::cout << "SendQuoteOrder(): " << ss.str() << std::endl;
         return;
@@ -141,7 +180,9 @@ void BLSFStrategy::SendQuoteOrder(const Instrument* instrument, int trade_size)
     OrderParams params(*instrument, 
         abs(trade_size),
         price, 
-        (instrument->type() == INSTRUMENT_TYPE_EQUITY) ? MARKET_CENTER_ID_NASDAQ : ((instrument->type() == INSTRUMENT_TYPE_OPTION) ? MARKET_CENTER_ID_CBOE_OPTIONS : MARKET_CENTER_ID_CME_GLOBEX),
+        (instrument->type() == INSTRUMENT_TYPE_EQUITY) ? MARKET_CENTER_ID_NASDAQ :
+            ((instrument->type() == INSTRUMENT_TYPE_OPTION) ? MARKET_CENTER_ID_CBOE_OPTIONS :
+            MARKET_CENTER_ID_CME_GLOBEX),
         (trade_size>0) ? ORDER_SIDE_BUY : ORDER_SIDE_SELL,
         ORDER_TIF_DAY,
         ORDER_TYPE_LIMIT);
