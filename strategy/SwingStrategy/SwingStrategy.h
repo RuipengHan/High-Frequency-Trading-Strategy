2e2af7f8dd097282d042ce008c7aefe4c2d4e28e
Tomoyoshi Kimura <student@illinois.edu>
2022-05-13 10:43:00 +0000

    Finished tuning Swing Strategy

--- strategy/SwingStrategy/SwingStrategy.h
+++ strategy/SwingStrategy/SwingStrategy.h
@@ -57,13 +57,29 @@ public:
         m_longWindow.clear();
     }
 
-    DesiredPositionSide Update(double val) {
+    DesiredPositionSide Update(double val, DesiredPositionSide currentTrend) {
+        double prev_back = m_shortWindow.back();
         m_shortWindow.push_back(val);
         m_longWindow.push_back(val);
+        if (currentTrend == DESIRED_POSITION_SIDE_LONG && prev_back > val) {
+            return DESIRED_POSITION_SIDE_FLAT;
+        }
+        if (currentTrend == DESIRED_POSITION_SIDE_SHORT && prev_back < val) {
+            return DESIRED_POSITION_SIDE_FLAT;
+        }
+
+        double price_diff = m_shortWindow.Mean() - m_longWindow.Mean();
+        // std::cout << price_diff / m_longWindow.Mean() << std::endl;
         if (m_shortWindow.Mean() > m_longWindow.Mean()){
-            return DESIRED_POSITION_SIDE_LONG;
+            if(price_diff / m_longWindow.Mean() < 0.000006) {
+                return DESIRED_POSITION_SIDE_LONG;
+            }
+            // cout << price_diff / m_longWindow.Mean() << endl;
         } else if (m_shortWindow.Mean() < m_longWindow.Mean()) {
-            return DESIRED_POSITION_SIDE_SHORT;
+            if (price_diff / m_longWindow.Mean() > -0.000006) {
+                return DESIRED_POSITION_SIDE_SHORT;
+            }
+            // cout << price_diff / m_longWindow.Mean() << endl;
         }
         return DESIRED_POSITION_SIDE_FLAT;
     }
